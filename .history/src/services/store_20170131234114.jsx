82682602e3e38322cf0089d692cfb870d808e4d8
Hongbo LU <gitawego@gmail.com>
2017-02-01 00:57:07 +0100

    chore(history): remove .history

--- .history/src/services/store_20170131234114.jsx
+++
@@ -1,7 +0,0 @@
-import 'isomorphic-fetch';
-export class Store {
-	static url = 'http://private-5d708-interviewfront.apiary-mock.com/transactions';
-	static getData() {
-		return fetch(url);
-	}
-}
--- .history/src/services/store_20170131235158.jsx
+++
@@ -1,6 +0,0 @@
-import 'isomorphic-fetch';
-export class Store {
-	static getData() {
-		return fetch('http://private-5d708-interviewfront.apiary-mock.com/transactions');
-	}
-}
